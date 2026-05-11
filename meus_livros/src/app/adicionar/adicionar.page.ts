a45df664f3fee0248e836312b200cd554b07c60c
Amanda <limaa9000@gmail.com>
2026-05-10 21:31:16 -0300

    Cores

--- meus_livros/src/app/adicionar/adicionar.page.ts
+++ meus_livros/src/app/adicionar/adicionar.page.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adicionar.page.scss'],
   standalone: false
 })
-export class AdicionarPage implements OnInit {
 
-  constructor() { }
+export class AdicionarPage {
+  // Variável que controla a exibição (começa como false porque o padrão é 'nao-leu')
+  foiLido: boolean = false;
 
-  ngOnInit() {
+  constructor() {}
+
+  statusChange(event: any) {
+    // Se o valor selecionado for 'leu', a variável fica true
+    this.foiLido = event.detail.value === 'leu';
   }
-
 }
